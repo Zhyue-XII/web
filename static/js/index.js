e26440ccc6990e690851848a570f0b725ffe4057
Zhyue-XII <1551985018@qq.com>
2018-08-16 16:51:00 +0800

    动画添加

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,10 @@
-$('.carousel').carousel({  interval: 3000})
+$('.carousel').carousel({interval: 5000})
 
 $(function () {
-    $("#mo-fang").css({"transform":"rotate(360deg)"})
-    $("#mo-title").css({"margin-top":"30px","opacity":"1","transform":"scale(1)"})
+    $("#mo-fang").css({"transform":"rotate(360deg)","transition-delay":".8s","opacity":"1"})
+    $("#mo-title").css({"opacity":"1","transform":"scale(1)","margin-top":"7%"})
     $("#first").css({background:'#f54e058c'})
+    $(".bg-service").css({"transform":"scale(1)","opacity":"1"})
 })
 
 $("#last-img").click(function () {$("#wapper").css({"margin-left":"-200%"})})
@@ -28,3 +29,15 @@ $("#last").click(function () {
     $("#first").css({background: 'gainsboro'})
     $("#next").css({background: 'gainsboro'})
 })
+
+$("#try-btn").click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+        if ($target.length) {
+            var targetOffset = $target.offset().top;
+            $('html,body').animate({scrollTop: targetOffset}, 800);
+            return false;
+        }
+    }
+});
