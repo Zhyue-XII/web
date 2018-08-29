4dbc0ab929b7961ac762c9b548b9ac2cf983061a
Zhyue-XII <1551985018@qq.com>
2018-08-29 15:55:00 +0800

    内容添加

--- static/js/index.js
+++ static/js/index.js
@@ -3,6 +3,64 @@ $(function () {
     $("#mo-title").css({"opacity":"1","transform":"scale(1)","margin-top":"7%"})
     $("#first").css({background:'#f54e058c'})
     $(".bg-service").css({"transform":"scale(1)","opacity":"1"})
+
+    $("#frm-data").bootstrapValidator({
+        message: 'This value is not valid',
+        excluded: 'disabled',//关键配置，表示只对于禁用域不进行验证，其他的表单元素都要验证
+
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',//显示验证成功或者失败时的一个小图标
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+
+        },
+
+        fields: {
+            name: {
+                message: '用户名不能为空',//默认提示信息
+                validators: {
+                    notEmpty: {
+                        message: '用户名必填不能为空'
+                    },
+                }
+            },
+            company: {
+                message: '用户名不能为空',//默认提示信息
+                validators: {
+                    notEmpty: {
+                        message: '公司名称必填不能为空'
+                    },
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: '邮件不能为空'
+                    },
+                    emailAddress: {
+                        message: '请输入正确的邮件地址如：123@qq.com'
+                    }
+                }
+            },
+            tel: {
+                message: 'The phone is not valid',
+                validators: {
+                    notEmpty: {
+                        message: '手机号码不能为空'
+                    },
+                    stringLength: {
+                        min: 11,
+                        max: 11,
+                        message: '请输入11位手机号码'
+                    },
+                    regexp: {
+                        regexp: /^1[3|5|8]{1}[0-9]{9}$/,
+                        message: '请输入正确的手机号码'
+                    }
+                }
+            },
+        }
+    })
 })
 
 $("#last-img").click(function () {$("#wapper").css({"margin-left":"-200%"})})
@@ -39,3 +97,12 @@ $("#try-btn").click(function () {
         }
     }
 });
+
+$("#sbm-btn").click(function () {
+    $("#frm-data").bootstrapValidator('validate');
+    var data_list = $("#frm-data").serialize();
+    if($("#frm-data").data('bootstrapValidator').isValid()){
+        console.log(data_list)
+        alert('提交')
+    }
+})
